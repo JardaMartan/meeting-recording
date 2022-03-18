262662839166dfacb29e6d401599087c98e7e8ee
Jaroslav Martan <jmartan@cisco.com>
2022-03-18 10:36:40 +0100

    comments

--- src/api_interactive.py
+++ src/api_interactive.py
@@ -1,3 +1,11 @@
+"""
+Interactive Webex API client for testing the integration access.
+Before running this, run the main code and pass through the OAuth process.
+Once the OAuth saves tokens in file, they will become accessible also
+to this code.
+
+run: python -i api_interactive.py
+"""
 from webexteamssdk import WebexTeamsAPI
 import oauth_grant_flow as oa
 oa.webex_token_key="recording_bot"
