efb871c5f4726cd1116303c6f31b63a6d9dbef69
Jaroslav Martan <jmartan@cisco.com>
2023-01-04 17:19:37 +0100

    path

--- src/__init__.py
+++ src/__init__.py
@@ -8,9 +8,10 @@ logger.setLevel(logging.DEBUG)
 
 path_root = Path(__file__).parents[2]
 sys.path.append(str(path_root))
-logger.info(f"path: {sys.path}")
+logging.error(f"path: {sys.path}")
+print(f"path: {sys.path}")
 
-from recording_bot import flask_app
+from src.recording_bot import flask_app
 
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     return func.WsgiMiddleware(flask_app.wsgi_app).handle(req, context)
