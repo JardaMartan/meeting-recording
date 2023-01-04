77efbf6aae2beaf4d34c2ad0b5b3b0d2eb154256
Jaroslav Martan <jmartan@cisco.com>
2023-01-04 16:07:26 +0100

    azure function

--- src/__init__.py
+++ src/__init__.py
@@ -3,17 +3,3 @@ from recording_bot import flask_app
 
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     return func.WsgiMiddleware(flask_app.wsgi_app).handle(req, context)
-
-
-"""
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-    uri=req.params['uri']
-    with flask_app.test_client() as c:
-        doAction = {
-            "GET": c.get(uri).data,
-            "POST": c.post(uri).data
-        }
-        resp = doAction.get(req.method).decode()
-        return func.HttpResponse(resp, mimetype='text/html')
-"""
