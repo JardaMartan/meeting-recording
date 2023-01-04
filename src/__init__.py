15b30f48544cc1526af7a3ccdde568d10710a466
Jaroslav Martan <jmartan@cisco.com>
2023-01-04 17:02:32 +0100

    import

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,10 @@
 import azure.functions as func
+from pathlib import Path
+import sys
+path_root = Path(__file__).parents[2]
+sys.path.append(str(path_root))
+logging.info(f"path: {sys.path}")
+
 from recording_bot import flask_app
 
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
