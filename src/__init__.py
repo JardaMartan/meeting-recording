4837a60adeb6aedc55d6f34dfb7eaafdc3b4ce10
Jaroslav Martan <jmartan@cisco.com>
2023-01-06 13:50:57 +0100

    file storage mount

--- src/__init__.py
+++ src/__init__.py
@@ -6,11 +6,16 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+if os.getenv("WEBSITE_CONTENTAZUREFILECONNECTIONSTRING", None) is not None:
+    from azure.storage.fileshare import ShareFileClient
+
+    file_client = ShareFileClient.from_connection_string(conn_str=os.getenv("WEBSITE_CONTENTAZUREFILECONNECTIONSTRING", share_name=os.getenv("WEBSITE_CONTENTSHARE"))
+    logging.error(f"{os.listdir('/')}")
+
 path_root = Path(__file__).parents[2]
 sys.path.append(str(path_root))
 logging.error(f"path: {sys.path}")
 print(f"path: {sys.path}")
-logging.error(f"{os.listdir('/')}")
 
 from .recording_bot import flask_app
 
