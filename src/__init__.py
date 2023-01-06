5c2d518a4266211dcc0f4789c63807daa2424e46
Jaroslav Martan <jmartan@cisco.com>
2023-01-06 13:28:46 +0100

    test

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 import azure.functions as func
 from pathlib import Path
-import sys
+import os,sys
 import logging
 
 logger = logging.getLogger(__name__)
@@ -10,6 +10,7 @@ path_root = Path(__file__).parents[2]
 sys.path.append(str(path_root))
 logging.error(f"path: {sys.path}")
 print(f"path: {sys.path}")
+logging.error(f"{os.listdir('/')}")
 
 from .recording_bot import flask_app
 
