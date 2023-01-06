4b2db71d8a6fb5a070040441d63790aca2ec0691
Jaroslav Martan <jmartan@cisco.com>
2023-01-06 14:42:05 +0100

    test function

--- src/__init__.py
+++ src/__init__.py
@@ -6,12 +6,6 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-if os.getenv("WEBSITE_CONTENTAZUREFILECONNECTIONSTRING", None) is not None:
-    from azure.storage.fileshare import ShareFileClient
-
-    file_client = ShareFileClient.from_connection_string(conn_str=os.getenv("WEBSITE_CONTENTAZUREFILECONNECTIONSTRING"), share_name=os.getenv("WEBSITE_CONTENTSHARE"), file_path="/")
-    logging.info(f"{os.listdir('/')}")
-
 path_root = Path(__file__).parents[2]
 sys.path.append(str(path_root))
 logging.info(f"path: {sys.path}")
@@ -28,6 +22,12 @@ from flask import Flask, redirect
 flask_app = Flask(__name__)
 
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    if os.getenv("WEBSITE_CONTENTAZUREFILECONNECTIONSTRING", None) is not None:
+        from azure.storage.fileshare import ShareFileClient
+
+        file_client = ShareFileClient.from_connection_string(conn_str=os.getenv("WEBSITE_CONTENTAZUREFILECONNECTIONSTRING"), share_name=os.getenv("WEBSITE_CONTENTSHARE"), file_path="/")
+        logging.info(f"{os.listdir('/')}")
+
     logging.info('Python HTTP trigger function processed a request.')
     return func.WsgiMiddleware(flask_app.wsgi_app).handle(req, context)
 
