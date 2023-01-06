3d0f42ddbad20eab751de39db8b808fba242461b
Jaroslav Martan <jmartan@cisco.com>
2023-01-06 14:53:15 +0100

    test func

--- src/__init__.py
+++ src/__init__.py
@@ -17,21 +17,27 @@ def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     return func.WsgiMiddleware(flask_app.wsgi_app).handle(req, context)
 """
 
-from flask import Flask, redirect
+from flask import Flask, redirect, make_response
 
 flask_app = Flask(__name__)
 
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-    if os.getenv("WEBSITE_CONTENTAZUREFILECONNECTIONSTRING", None) is not None:
-        from azure.storage.fileshare import ShareFileClient
-
-        file_client = ShareFileClient.from_connection_string(conn_str=os.getenv("WEBSITE_CONTENTAZUREFILECONNECTIONSTRING"), share_name=os.getenv("WEBSITE_CONTENTSHARE"), file_path="/")
-        logging.info(f"{os.listdir('/')}")
-
     logging.info('Python HTTP trigger function processed a request.')
     return func.WsgiMiddleware(flask_app.wsgi_app).handle(req, context)
 
 @flask_app.route("/")
 def root():
     logging.info("request received")
-    return redirect("https://www.cisco.com", code = 301)
+    if os.getenv("WEBSITE_CONTENTAZUREFILECONNECTIONSTRING", None) is not None:
+        from azure.storage.fileshare import ShareFileClient
+
+        file_client = ShareFileClient.from_connection_string(conn_str=os.getenv("WEBSITE_CONTENTAZUREFILECONNECTIONSTRING"), share_name=os.getenv("WEBSITE_CONTENTSHARE"), file_path="my_path")
+
+    lst1 = os.listdir('.')
+    lst2 = os.listdir('my_path')
+    
+    response = make_response(f"list1: {lst1}\nlist2: {lst2}", 200)
+    response.mimetype = "text/plain"
+    return response
+
+    # return redirect("https://www.cisco.com", code = 301)
